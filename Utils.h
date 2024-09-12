e96c38ad5d2f52085e09193157656605362d90f8
Ahsan Ali <ahsan145ali@gmail.com>
2024-09-12 13:31:18 +0100

    Added comments for clarity

--- Utils.h
+++ Utils.h
@@ -15,13 +15,48 @@ typedef enum {
 } ReturnStatus;
 
 
+/// @brief Get string input from user and sanitize it
+/// @param prompt text to display to user
+/// @return a pointer to a character string
 char* stringInput(char prompt[]);
+
+/// @brief Clear any newline character left on buffer after input
 void clearInputBuffer(void);
+
+/// @brief get character input from user and sanatize it 
+/// @param prompt text to display to user
+/// @return a character
 char characterInput(char prompt[]);
+
+/// @brief converts string into format that is accepted by the server
+/// @param st string to be converted 
+/// @return a char pointer to the converted string
 char* prepareStringForServer(char *st);
+
+/// @brief checks if the array returned from server is all true
+/// @param resultArray array containing 1 and 0
+/// @return integer to represent result
 int checkAllCorrect(char* resultArray);
+
+/// @brief checks if the current guess is correct
+/// @param wordArray array that has the words that is being formed as the user guesses
+/// @param resultArray array that is returned from the server containing 1 and 0
+/// @param guessedWord the current guess
+/// @return an integer to represent result
 int checkGuess(char* wordArray , char* resultArray , char guessedWord);
+
+/// @brief adds current guess to the guess list 
+/// @param guesslist array that holds all the guesses made by the user
+/// @param guessedWord current guess
+/// @return int to represent result
 int addToGuessList(char **guesslist , char guessedWord);
+
+/// @brief Allocate memory
+/// @param ptr pointer to the array to assign memory to
+/// @return int to represent result
 int allocateMemory(char **ptr);
+
+/// @brief prints hangman representation
+/// @param lives current lives of the user
 void Hungman(int lives);
 #endif // UTILS_H
